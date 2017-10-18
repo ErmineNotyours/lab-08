c2552c0a7a819c96e6b2cb88ee1d2a4f99328668
ErmineNotyours <arthurallen@centurylink.net>
2017-10-17 20:20:34 -0700

    added and debugged alteration that pulls store data from arrays, to allow flexibility to add stores with forms

--- appSales2.js
+++ appSales2.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log('Top of script.');
+//console.log('Top of script.');
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 //Need to: put store info into arrays, so they can be expanded (pushed to) by forms.
 var storeName = ['First and Pike','SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
@@ -82,26 +82,27 @@ for(var l = 0; l < hours.length; l++){
 tblBody.appendChild(row);
 console.log('row = ', row);
 
-// for (location = 0; location < storeName.length; location++){
-//   console.log('In the for location loop, location ', location);
-//   var calcStore = new Store(storeName[location], minCustPerHourArr[location], maxCustPerHourArr[location], avgCookeSoldPerHourArr[location]);
-//   calcStore.render();
-// }
+//New code for accessing object by arrays. Needed for flexibility for adding stores by form
+for (var loc = 0; loc < storeName.length; loc++){
+  console.log('In the for location loop, location ', location);
+  var calcStore = new Store(storeName[loc], minCustPerHourArr[loc], maxCustPerHourArr[loc], avgCookeSoldPerHourArr[loc]);
+  calcStore.render();
+}
 
-var firstAndPike = new Store('First and Pike', 23, 65, 6.3);
-firstAndPike.render();
-
-var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
-seaTacAirport.render();
-
-var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
-seattleCenter.render();
-
-var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
-capitolHill.render();
-
-var alKi = new Store('Alki', 2, 16, 4.6);
-alKi.render();
+// var firstAndPike = new Store('First and Pike', 23, 65, 6.3);
+// firstAndPike.render();
+//
+// var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
+// seaTacAirport.render();
+//
+// var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
+// seattleCenter.render();
+//
+// var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
+// capitolHill.render();
+//
+// var alKi = new Store('Alki', 2, 16, 4.6);
+// alKi.render();
 
 //Column total row here
 var grandTotal = 0;
