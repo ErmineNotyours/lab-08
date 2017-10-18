bc33ef030590facfc3f248bc33f5b0175983232d
ErmineNotyours <arthurallen@centurylink.net>
2017-10-17 18:38:11 -0700

    Converted sales2 to run on arrays, but it is not opening in browser

--- appSales2.js
+++ appSales2.js
@@ -2,6 +2,10 @@
 console.log('Top of script.');
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 //Need to: put store info into arrays, so they can be expanded (pushed to) by forms.
+var storeName = ['First and Pike','SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+var minCustPerHour = [23, 3, 11, 20, 2];
+var maxCustPerHour = [65, 24, 38, 38, 16];
+var avgCookeSoldPerHour = [6.3, 1.2, 3.7, 2.3, 4.6];
 
 // get the reference for the body
 var body = document.getElementsByTagName('body')[0];
@@ -66,17 +70,23 @@ for(var l = 0; l < hours.length; l++){
 }
 tblBody.appendChild(row);
 console.log('row = ', row);
-var firstAndPike = new Store('First and Pike', 23, 65, 6.3);
-firstAndPike.render();
 
-var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
-seaTacAirport.render();
+for (location = 0; location < storeName.length; location++){
+  var calcStore = new Store(storeName[location], minCustPerHour[location], maxCustPerHour[location], avgCookeSoldPerHour[location]);
+  calcStore.render();
+}
 
-var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
-seattleCenter.render();
-
-var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
-capitolHill.render();
-
-var alKi = new Store('Alki', 2, 16, 4.6);
-alKi.render();
+// var firstAndPike = new Store('First and Pike', 23, 65, 6.3);
+// firstAndPike.render();
+//
+// var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
+// seaTacAirport.render();
+//
+// var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
+// seattleCenter.render();
+//
+// var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
+// capitolHill.render();
+//
+// var alKi = new Store('Alki', 2, 16, 4.6);
+// alKi.render();
