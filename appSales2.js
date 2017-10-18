71547019652df19fb350fc8177a6786512ecae5a
ErmineNotyours <arthurallen@centurylink.net>
2017-10-17 19:31:02 -0700

    added rough row totals

--- appSales2.js
+++ appSales2.js
@@ -3,9 +3,9 @@ console.log('Top of script.');
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 //Need to: put store info into arrays, so they can be expanded (pushed to) by forms.
 var storeName = ['First and Pike','SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
-var minCustPerHour = [23, 3, 11, 20, 2];
-var maxCustPerHour = [65, 24, 38, 38, 16];
-var avgCookeSoldPerHour = [6.3, 1.2, 3.7, 2.3, 4.6];
+var minCustPerHourArr = [23, 3, 11, 20, 2];
+var maxCustPerHourArr = [65, 24, 38, 38, 16];
+var avgCookeSoldPerHourArr = [6.3, 1.2, 3.7, 2.3, 4.6];
 
 // get the reference for the body
 var body = document.getElementsByTagName('body')[0];
@@ -44,14 +44,20 @@ function Store (name, minCustPerHour, maxCustPerHour, avgCookeSoldPerHour, calcR
     //console.log('Appended left header, about to enter calcCookiesSoldByHour');
     this.calcRandCustByHour();
     this.calcCookiesSoldByHour();
+    var xTotal = 0;
     for(var k = 0; k < hours.length; k++){
       var salesTd = document.createElement('td');
       salesTd.textContent = this.cookiesSoldByHour[k];
+      xTotal = xTotal + this.cookiesSoldByHour[k];
       row.appendChild(salesTd);
     }
-    // Need to add totals, bottom and right side!
+    //Attempting to add row total here
+    var salesTd = document.createElement('td');
+    salesTd.textContent = xTotal;
+    row.appendChild(salesTd);
 
     tblBody.appendChild(row);
+
   };
 };
 
@@ -71,22 +77,23 @@ for(var l = 0; l < hours.length; l++){
 tblBody.appendChild(row);
 console.log('row = ', row);
 
-for (location = 0; location < storeName.length; location++){
-  var calcStore = new Store(storeName[location], minCustPerHour[location], maxCustPerHour[location], avgCookeSoldPerHour[location]);
-  calcStore.render();
-}
+// for (location = 0; location < storeName.length; location++){
+//   console.log('In the for location loop, location ', location);
+//   var calcStore = new Store(storeName[location], minCustPerHourArr[location], maxCustPerHourArr[location], avgCookeSoldPerHourArr[location]);
+//   calcStore.render();
+// }
 
-// var firstAndPike = new Store('First and Pike', 23, 65, 6.3);
-// firstAndPike.render();
-//
-// var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
-// seaTacAirport.render();
-//
-// var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
-// seattleCenter.render();
-//
-// var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
-// capitolHill.render();
-//
-// var alKi = new Store('Alki', 2, 16, 4.6);
-// alKi.render();
+var firstAndPike = new Store('First and Pike', 23, 65, 6.3);
+firstAndPike.render();
+
+var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
+seaTacAirport.render();
+
+var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
+seattleCenter.render();
+
+var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
+capitolHill.render();
+
+var alKi = new Store('Alki', 2, 16, 4.6);
+alKi.render();
